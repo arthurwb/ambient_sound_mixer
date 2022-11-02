189bf075d5e530068bacad8806fa07aa5d3a88fe
arthurwb <arthurwb@mail.uc.edu>
2022-11-01 22:33:05 -0400

    started creating desktop homepage, added new fonts

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.route('/')
 })
 
 //  listening on port 8081
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+var server = app.listen(8080, function () {
+   var host = server.address().address;
+   var port = server.address().port;
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://%s:%s", host, port);
 })
